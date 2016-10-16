2feb2c30d3231c6a9a1fdfba14726d40ce92c1d4
kendeng <kendeng@KingGarcia.local>
2016-10-16 12:45:07 +0800

    Commit for Last Lab Activity

---
+++ SemiCompiler/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args){
+        int i = 0;
+		if(i==1){
+			System.out.println("kumusta mundo"); System.labas.iprint("kumusta mundo");
+			System.out.println("System.labas.iprint");
+		}
+	}
+}
--- SemiCompiler/src/semicompiler/SemiCompiler.java
+++ SemiCompiler/src/semicompiler/SemiCompiler.java
@@ -5,21 +5,55 @@
  */
 package semicompiler;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author kcgarcia
  */
 public class SemiCompiler {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException, IOException {
+        Scanner sc = new Scanner(new FileReader("input.txt"));
+
+        //reads the file "input.txt"
+        PrintWriter writer = new PrintWriter("Test.java", "UTF-8");
+
+        while (sc.hasNext()) {
+            String str = sc.nextLine();
+            //replaces kung with if
+            str = str.replaceAll("kung", "if");
+            
+            String digitpattern = "(System.labas.iprint\\(\".*\"\\))";
+            Pattern digit = Pattern.compile(digitpattern);
+            Matcher digitmatcher = digit.matcher(str);
+            if(digitmatcher.find()){
+                str = str.replaceFirst("System.labas.iprint", "System.out.println");
+            }
+
+//            str = str.replaceAll("(System.labas.iprint\\(\"[a-z]*\"\\))", "System.out.println\\(\"[a-z]*\"\\)");
+            //System.out.println("");
+            writer.println(str);
+        }
         
-        System.out.println("TEST INITIAL COMMIT 2");
-        System.out.println("TEST INITIAL COMMIT 2");
-        System.out.println("TEST COMMIT COMMIT 3 NEWEST");
+        writer.close();
+        List cmdAndArgs = Arrays.asList("cmd", "/c", "run.bat");
+        File dir = new File(System.getProperty("user.dir"));
+
+        ProcessBuilder pb = new ProcessBuilder(cmdAndArgs);
+        pb.directory(dir);
+        Process p = pb.start();
     }
     
 }
